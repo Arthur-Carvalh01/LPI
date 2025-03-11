da59dfabbb317a1c6f65a30bfd08dab427b5343e
José Arthur <arthurjoseb2017@gmail.com>
2025-03-11 10:39:17 -0300

    Atualizar o Gerenciador.cpp

--- Gerenciador.cpp
+++ Gerenciador.cpp
@@ -8,4 +8,16 @@
 
 class Gerenciador{
     private :
+	public:
+		void menu(){
+			cout << "BEM VINDO A TOURO TEC" << endl;
+        	cout << "1-Computadores" << endl;
+        	cout << "2-Notebooks" << endl;
+        	cout << "3-Servicos" << endl;
+        	cout << "4-Sair" << endl;
+		}
+
+		void menu_opcao(){
+			
+		}
 };
